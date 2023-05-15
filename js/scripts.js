67b1972c04a2de7e302da34f6b5063925c8e0124
romeopreda <55390233+romeopreda@users.noreply.github.com>
2023-05-15 23:01:52 +0200

    ascundelink

--- js/scripts.js
+++ js/scripts.js
@@ -53,6 +53,23 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+      var linkAcasa = document.getElementById("linkAcasa");
+      var hrefAcasa = linkAcasa.getAttribute("href");
+      linkAcasa.setAttribute("href", hrefAcasa.split("#")[0]);
+
+      var linkContact = document.getElementById("linkContact");
+      var hrefContact = linkContact.getAttribute("href");
+      linkContact.setAttribute("href", hrefContact.split("#")[0]);
+
+       var linkContact = document.getElementById("linkMagazin");
+       var hrefContact = linkContact.getAttribute("href");
+       linkContact.setAttribute("href", hrefContact.split("#")[0]);
+
+       var linkContact = document.getElementById("linkInformatii");
+       var hrefContact = linkContact.getAttribute("href");
+       linkContact.setAttribute("href", hrefContact.split("#")[0]);
+
+
 
 
 });
